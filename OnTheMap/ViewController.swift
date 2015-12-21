e79117857939222740542182131d1ea4e4f3173c
Mrudul Pendharkar <mrudul.pendharkar@gmail.com>
2015-12-21 22:07:19 +0530

    Initial UI for Login Screen

--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    var backgroundGradient:CAGradientLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        configureUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func configureUI(){
+        /* Configure background gradient */
+        self.view.backgroundColor = UIColor.clearColor()
+        let colorTop = UIColor(red: 0.748, green: 0.254, blue: 0.0, alpha: 1.0).CGColor
+        let colorBottom = UIColor(red: 0.954, green: 0.054, blue: 0.08, alpha: 1.0).CGColor
+        self.backgroundGradient = CAGradientLayer()
+        self.backgroundGradient!.colors = [colorTop, colorBottom]
+        self.backgroundGradient!.locations = [0.0, 1.0]
+        self.backgroundGradient!.frame = view.frame
+        self.view.layer.insertSublayer(self.backgroundGradient!, atIndex: 0)
+        
+        /* Configure header text label */
+//        headerTextLabel.font = UIFont(name: "AvenirNext-Medium", size: 24.0)
+//        headerTextLabel.textColor = UIColor.whiteColor()
+//        
+        /* Configure email textfield */
+        let emailTextFieldPaddingViewFrame = CGRectMake(0.0, 0.0, 13.0, 0.0);
+        let emailTextFieldPaddingView = UIView(frame: emailTextFieldPaddingViewFrame)
+        usernameTextField.leftView = emailTextFieldPaddingView
+        usernameTextField.leftViewMode = .Always
+        usernameTextField.font = UIFont(name: "AvenirNext-Medium", size: 17.0)
+        usernameTextField.backgroundColor = UIColor(red: 0.702, green: 0.863, blue: 0.929, alpha:1.0)
+        usernameTextField.textColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: usernameTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        usernameTextField.tintColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        
+        /* Configure password textfield */
+        let passwordTextFieldPaddingViewFrame = CGRectMake(0.0, 0.0, 13.0, 0.0);
+        let passwordTextFieldPaddingView = UIView(frame: passwordTextFieldPaddingViewFrame)
+        passwordTextField.leftView = passwordTextFieldPaddingView
+        passwordTextField.leftViewMode = .Always
+        passwordTextField.font = UIFont(name: "AvenirNext-Medium", size: 17.0)
+        passwordTextField.backgroundColor = UIColor(red: 0.702, green: 0.863, blue: 0.929, alpha:1.0)
+        passwordTextField.textColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        passwordTextField.tintColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        
+        loginButton.tintColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        loginButton.backgroundColor = UIColor(red: 0.7, green: 0.9, blue: 0.4, alpha: 0.9)
+        
+        /* Configure debug text label */
+//        headerTextLabel.font = UIFont(name: "AvenirNext-Medium", size: 20)
+//        headerTextLabel.textColor = UIColor.whiteColor()
 
+    }
 }
 
